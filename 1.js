c10dcd064d727ce1f7c9654c86b2267b601f9539
Eos <86279093+JosueEnrique@users.noreply.github.com>
2024-07-30 16:21:42 -0600

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,17 +1,1 @@
-// Escribe un programa que solicite al usuario su nombre y apellido por separado//
-//y luego muestre un mensaje de saludo concatenando ambos.//
 
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Ingrese su nombre: ', (nombre) => {
-  rl.question('Ingrese su apellido: ', (apellido) => {
-    const saludo = `Hola, ${nombre} ${apellido}!`;
-    console.log(saludo);
-    rl.close();
-  });
-});
